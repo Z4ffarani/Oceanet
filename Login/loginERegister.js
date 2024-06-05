1d338c6d00bf0d0f11ef48e706d5e58eb44fa86c
sannunez <sftwr.gsantosnunes210@gmail.com>
2024-06-04 22:39:42 -0300

    Removendo códigos desnecessários

--- Login/loginERegister.js
+++ Login/loginERegister.js
@@ -1,13 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     document.getElementById("entrar").addEventListener("click", redirect);
-//     document.getElementById("registrar").addEventListener("click", redirect);
-
-//     function redirect() {
-//         window.location.href = "../Site/index.html";
-//     }
-// });
-
-
 function armazenarDados(){
     var email = document.getElementById("email").value
     var senha = document.getElementById("senha").value
