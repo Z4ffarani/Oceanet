bcefeb2db05bfaeebea20a1e714b6fe8c1e0c7ff
sannunez <sftwr.gsantosnunes210@gmail.com>
2024-06-06 05:35:08 -0300

    carrossel de imagens pronto

--- Site/script.js
+++ Site/script.js
@@ -10,33 +10,42 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "../Login/login.html";
     }
 
-    
+    // programação carrossel //
+    const carrossel = document.getElementById('carrossel');
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    let slideAtual = 0
+
+    document.getElementById("proxBtn").addEventListener("click", function(){
+        if (slideAtual < totalSlides - 1){
+            slideAtual = slideAtual + 1;
+            atualizarCarrossel()
+        }
+        else{
+            slideAtual = 0;
+            atualizarCarrossel();
+        }
+    });
+
+    document.getElementById("voltBtn").addEventListener("click", function(){
+        if (slideAtual > 0 ){
+            slideAtual = slideAtual - 1;
+            atualizarCarrossel()
+        }
+        else{
+            slideAtual = totalSlides - 1;
+            atualizarCarrossel();
+        }
 });
 
+function atualizarCarrossel(){
+    const troca = -slideAtual * 500;
+    carrossel.style.transform='translateX(' + troca + 'px)';
+}
+});
+
+// programação gaveta //
 function alternarGaveta(){
-    var menuGaveta = document.getElementById('menuGaveta')
+    var menuGaveta = document.getElementById('menuGaveta');
     menuGaveta.classList.toggle('aberto');
 }
-
-// const carrossel = document.getElementById('carrossel')
-// const slides = document.querySelectorAll(".slideCarrossel")
-// const totalSlides = slides.length
-// var slideAtual = 0;
-
-// document.getElementById("subir-btn").addEventListener("click", function(){
-//     if(slideAtual < totalSlides){
-//         slideAtual = slideAtual + 1;
-//         atualizarCarrossel()
-//     }
-// });
-
-// document.getElementById("descer-btn").addEventListener("click", function(){
-//     if(slideAtual > totalSlides)
-//         slideAtual = slideAtual - 1;
-//         atualizarCarrossel()
-// })
-
-// function atualizarCarrossel(){
-//     const alternar = -slideAtual * 400;
-//     carrossel.style.top = alternar + "px";
-// }
