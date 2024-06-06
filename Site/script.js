b439d6ccb951f461b0d301f011f5c291f72fd4a2
sannunez <sftwr.gsantosnunes210@gmail.com>
2024-06-06 02:50:03 -0300

    Menu de navegação entre seções concluido

--- Site/script.js
+++ Site/script.js
@@ -9,9 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
     function redirectToLogin() {
         window.location.href = "../Login/login.html";
     }
+
+    
 });
 
 function alternarGaveta(){
     var menuGaveta = document.getElementById('menuGaveta')
     menuGaveta.classList.toggle('aberto');
 }
+
+// const carrossel = document.getElementById('carrossel')
+// const slides = document.querySelectorAll(".slideCarrossel")
+// const totalSlides = slides.length
+// var slideAtual = 0;
+
+// document.getElementById("subir-btn").addEventListener("click", function(){
+//     if(slideAtual < totalSlides){
+//         slideAtual = slideAtual + 1;
+//         atualizarCarrossel()
+//     }
+// });
+
+// document.getElementById("descer-btn").addEventListener("click", function(){
+//     if(slideAtual > totalSlides)
+//         slideAtual = slideAtual - 1;
+//         atualizarCarrossel()
+// })
+
+// function atualizarCarrossel(){
+//     const alternar = -slideAtual * 400;
+//     carrossel.style.top = alternar + "px";
+// }
